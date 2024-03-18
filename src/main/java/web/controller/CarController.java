ebcabb9e0b3cff0cf7c003c79a200db5cd4bb8af
Dmitry Ivanov <dmitryivanov92@gmail.com>
2024-03-18 22:30:09 +0400

    исправлены ошибки задание 2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 
 @Controller
