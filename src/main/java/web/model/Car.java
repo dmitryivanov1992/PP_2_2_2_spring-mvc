7299f2585854d7c3e91cf773cece9f9c48a6c619
Dmitry <dmitryivanov92@gmail.com>
2024-03-18 16:29:30 +0400

    задача 2.2.2 выполнена

--- src/main/java/web/model/Car.java
+++ src/main/java/web/model/Car.java
@@ -1,7 +1,5 @@
 package web.model;
 
-import org.springframework.stereotype.Component;
-
 public class Car {
     public String model;
     public int productionYear;
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -5,5 +5,5 @@ import web.model.Car;
 import java.util.List;
 
 public interface CarService {
-    public List<Car> carsListCounted(Integer count);
+    List<Car> carsListCounted(Integer count);
 }
